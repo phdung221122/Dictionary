c31a4974a6b9642b0cb37715414e57cb417c9aa0
Uncl3L4k3 <dungpham21022003@gmail.com>
2022-04-24 14:58:20 +0700

    thêm và test hàm runApplication()

--- src/main/java/DictionaryApplication.java
+++ src/main/java/DictionaryApplication.java
@@ -3,16 +3,24 @@ import java.awt.*;
 
 public class DictionaryApplication extends JFrame {
     public DictionaryApplication(){
-        JLabel label = new JLabel("fuck word", SwingConstants.CENTER);
-        label.setForeground(Color.BLUE);
-        label.setFont(new Font(Font.SERIF , Font.ITALIC, 40));
-        this.setSize(400, 400);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.add(label);
+
+    }//constructor
+
+    public void runApplication(){
+        JLabel label = new JLabel("just test", SwingConstants.CENTER);// hiển thị từ muốn đưa ra màn hình
+        label.setForeground(Color.BLUE);//chọn màu
+        label.setFont(new Font(Font.SERIF , Font.ITALIC, 40));//chọn font
+        this.setSize(400, 400);//chọn size
+        this.setTitle("just test");//hiển thị tên giao diện
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//tắt hoàn toàn chương trình khi close
+        this.setLocationRelativeTo(null);//chỉnh cửa sổ giữa màn hình
+        this.add(label);//tương tự hàm renderCopy trong SDL c++
+        this.setVisible(true);//hiện cửa sổ ra màn hình (tương tự hàm renderPresent trong SDL c++)
     }
 
+
     public static void main(String[] args) {
-        new DictionaryApplication().setVisible(true);
+        DictionaryApplication test = new DictionaryApplication();
+        test.runApplication();
     }
 }
