e171229bb7b1f7ad91b28e2e6ff43866367e5aa8
phdung221122 <101117353+phdung221122@users.noreply.github.com>
2022-04-24 11:58:24 +0700

    hoan thanh lookup

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Word {
     private String word_target;//từ mới tiếng anh
     private String word_explain;//giải nghĩa tiếng việt
@@ -21,5 +23,18 @@ public class Word {
     public Word(String x, String y) {
         this.word_target = x;
         this.word_explain = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return word_target.equals(word.word_target);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word_target);
     }
 }
