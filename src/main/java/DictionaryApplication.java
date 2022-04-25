140e7cb161cbd1ff1e656db052ab59195e254629
Uncl3L4k3 <dungpham21022003@gmail.com>
2022-04-25 13:33:25 +0700

    thêm các class để tạo thành hành động cho nút search

--- src/main/java/DictionaryApplication.java
+++ src/main/java/DictionaryApplication.java
@@ -1,41 +1,52 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class DictionaryApplication extends JFrame {
+    private searchWord word;
+    private JButton searchButton;
+    ActionListener ac = new inputWord(this);
+
 
     //constructor
     public DictionaryApplication() {
+        this.word = new searchWord();
+        this.runApplication();
+    }
+
+    //chạy giao diện đồ họa
+    public void runApplication() {
         this.setTitle("Dictionary");//tên giao diện
         this.setSize(400, 400);//chọn kích cỡ cho giao diện
         this.setLocationRelativeTo(null);//căn giữa cửa sổ giao diện
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//tắt hoàn toàn chương trình khi close
-
     }
 
-
-    public void runApplication() {
-        /*JLabel label = new JLabel("just test", SwingConstants.CENTER);// hiển thị từ muốn đưa ra màn hình
-        label.setForeground(Color.BLUE);//chọn màu
-        label.setFont(new Font(Font.SERIF , Font.ITALIC, 40));//chọn font và cỡ chữ
-        this.add(label);//tương tự hàm renderCopy trong SDL c++*/
-        JTextField enterYourWord = new JTextField(16);//tạo 1 khoảng để nhập từ
+    //tra cứu từ
+    public void searchWord(){
+        JTextField enterYourWord = new JTextField(60);//tạo 1 khoảng để nhập từ
 
 
         JPanel wordPanel = new JPanel();//tạo 1 panel để phân các khu vực
         wordPanel.setLayout(new BorderLayout());
-        wordPanel.add(enterYourWord);
-        this.add(wordPanel, BorderLayout.CENTER);
+        wordPanel.add(enterYourWord, BorderLayout.CENTER);
 
-        FlowLayout aButton = new FlowLayout(FlowLayout.CENTER, 50, 50);//tạo nút bấm
-        this.setLayout(new FlowLayout());
-        JButton searchButton = new JButton("Search");//nút search
-        this.add(searchButton);
+
+        searchButton = new JButton("Search");//nút search
+        searchButton.addActionListener(ac);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BorderLayout());
-
+        buttonPanel.setLayout(new FlowLayout());
         buttonPanel.add(searchButton);
+
+
+        JLabel answer = new JLabel("just test", JLabel.CENTER);
+
+
+        this.setLayout(new BorderLayout());
+        this.add(wordPanel, BorderLayout.NORTH);
         this.add(buttonPanel, BorderLayout.CENTER);
+        this.add(answer, BorderLayout.SOUTH);
 
         this.setVisible(true);//hiện cửa sổ ra màn hình (tương tự hàm renderPresent trong SDL c++)
     }
@@ -43,7 +54,6 @@ public class DictionaryApplication extends JFrame {
 
     public static void main(String[] args) {
         DictionaryApplication test = new DictionaryApplication();
-        test.runApplication();
-
+        test.searchWord();
     }
 }
